cf0cb2f4606a04677937cfdd292b66c22ce8889f
Ivan Helsing <60290995+Ivan-Helsing@users.noreply.github.com>
2025-03-09 00:14:46 -0500

    BootstrapInstaller created. CoroutineRunner interface created.

---
+++ src/frozen-hell/Assets/Code/Infrastructure/Installers/BootstrapInstaller.cs
@@ -0,0 +1,23 @@
+﻿using Code.Infrastructure.Coroutines;
+using Zenject;
+
+namespace Code.Infrastructure.Installers
+{
+  public class BootstrapInstaller : MonoInstaller, IInitializable, ICoroutineRunner
+  {
+    public void Initialize()
+    {
+      
+    }
+
+    public override void InstallBindings()
+    {
+      BindInstaller();
+    }
+
+    private void BindInstaller()
+    {
+      Container.BindInterfacesTo<BootstrapInstaller>().FromInstance(this).AsSingle();
+    }
+  }
+}
