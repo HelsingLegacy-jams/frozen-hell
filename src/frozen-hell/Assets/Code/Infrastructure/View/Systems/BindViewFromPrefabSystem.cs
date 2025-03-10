81d15185fa71a3045bde457c5ff6521e6a6e35fa
Ivan Helsing <60290995+Ivan-Helsing@users.noreply.github.com>
2025-03-09 21:57:10 -0400

    BindViewFeature and systems created.

---
+++ src/frozen-hell/Assets/Code/Infrastructure/View/Systems/BindViewFromPrefabSystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Code.Infrastructure.View.Factory;
+using Entitas;
+
+namespace Code.Infrastructure.View.Systems
+{
+  public class BindViewFromPrefabSystem : IExecuteSystem
+  {
+    private readonly IGroup<GameEntity> _entities;
+    private readonly IEntityViewFactory _factory;
+    private readonly List<GameEntity> _buffer = new (32);
+
+    public BindViewFromPrefabSystem(GameContext game, IEntityViewFactory factory)
+    {
+      _factory = factory;
+      _entities = game.GetGroup(GameMatcher
+        .AllOf(
+          GameMatcher.ViewPrefab, 
+          GameMatcher.WorldPosition)
+        .NoneOf(GameMatcher.View));
+    }
+
+    public void Execute()
+    {
+      foreach (GameEntity entity in _entities.GetEntities(_buffer))
+      {
+        _factory.CreateViewForEntityFromPrefab(entity);
+      }
+    }
+  }
+}
