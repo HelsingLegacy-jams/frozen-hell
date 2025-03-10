4dea133d5949698a3262a5e7bac263f8fe6f16a6
Ivan Helsing <60290995+Ivan-Helsing@users.noreply.github.com>
2025-03-09 21:53:51 -0400

    Collisions registered for Entity

--- src/frozen-hell/Assets/Code/Infrastructure/View/EntityBehaviour.cs
+++ src/frozen-hell/Assets/Code/Infrastructure/View/EntityBehaviour.cs
@@ -1,12 +1,20 @@
-﻿using Code.Infrastructure.View.Registrars;
+﻿using Code.Gameplay.Common.Collisions;
+using Code.Infrastructure.View.Registrars;
 using UnityEngine;
+using Zenject;
 
 namespace Code.Infrastructure.View
 {
   public class EntityBehaviour : MonoBehaviour, IEntityView
   {
+    private ICollisionRegistry _collisions;
+    
     private GameEntity _entity;
     public GameEntity Entity => _entity;
+
+    [Inject]
+    public void Construct(ICollisionRegistry collisions) => 
+      _collisions = collisions;
 
     public void SetEntity(GameEntity entity)
     {
@@ -16,12 +24,19 @@ namespace Code.Infrastructure.View
 
       foreach (IEntityComponentRegistrar registrar in GetComponentsInChildren<IEntityComponentRegistrar>()) 
         registrar.Register();
+      
+      foreach (Collider collider in GetComponentsInChildren<Collider>()) 
+        _collisions.Register(collider.GetInstanceID(), _entity);
     }
 
     public void ReleaseEntity()
     {
       foreach (IEntityComponentRegistrar registrar in GetComponentsInChildren<IEntityComponentRegistrar>()) 
         registrar.Unregister();
+      
+      foreach (Collider collider in GetComponentsInChildren<Collider>()) 
+        _collisions.Unregister(collider.GetInstanceID());
+
       
       _entity.Release(this);
       _entity = null;
